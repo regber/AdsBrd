3e44cfb45b8122139c302def651a18550061909d
Regber <Regber.888@gmail.com>
2022-01-23 18:12:13 +0300

    Add Cryprograpy

---
+++ AdsBoard/Model/DBModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace AdsBoard.Model
+{
+    class DBModel
+    {
+
+    }
+    class Account
+    {
+
+
+        public int Id { get; set; }
+        public string Login { get; set; }
+        private string _Password;
+        public string Password 
+        { 
+            get
+            {
+                return Common.Cryprograpy.Decrypt(_Password);
+            }
+            set
+            {
+                _Password = Common.Cryprograpy.Encrypt(value);
+            }
+        }
+
+
+    }
+}
